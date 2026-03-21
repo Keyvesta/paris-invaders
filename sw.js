7fff5afc0263c08f18f7da18c9fb2a506e49f550
Keyvesta <marcemirian@hotmail.com>
2026-03-21 22:24:58 +0100

    v14: bump cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Service Worker - Paris Invaders V7
-const CACHE = 'paris-invaders-v13';
+// Service Worker - Paris Invaders V14
+const CACHE = 'paris-invaders-v14';
 const STATIC = [
   './',
   './index.html',
